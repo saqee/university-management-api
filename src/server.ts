eb2b99b4490ee25bfeea7a0c02d81aee203f0ec5
saqee <saqeeb108@gmail.com>
2023-12-30 21:33:36 +0100

    add cors and parser

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose"
+import app from "./app"
+import config from "./config/index"
+export const database = async () => {
+  try {
+    mongoose.connect(config.db as string)
+    console.log("db also connected")
+
+    app.listen(config.port, () => {
+      console.log("server is connect")
+    })
+  } catch (error) {
+    console.log("database error")
+  }
+}
+
+database()
